bbdd3da2937849398bc883113e7018d4a392714f
mitani1993 <fytim55@gmail.com>
2021-03-24 21:02:59 +0900

    recipeテーブル作成

--- db/migrate/20210324120116_create_recipes.rb
+++ db/migrate/20210324120116_create_recipes.rb
@@ -1,7 +1,11 @@
 class CreateRecipes < ActiveRecord::Migration[6.0]
   def change
     create_table :recipes do |t|
-
+      t.string :title,             null: false
+      t.text :text,                null: false
+      t.integer :category_id,      null:false
+      t.integer :time_required_id, null: false
+      t.references  :user,         foreign_key: true
       t.timestamps
     end
   end
